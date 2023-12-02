2242850fe701f970ec5e34316dddbe1f0a3b66ff
Alexandre Do-o Almeida <aleqsd@gmail.com>
2023-12-02 11:14:25 +0100

    Day 2

--- advent_1.2.py
+++ advent_1.2.py
@@ -1,6 +1,20 @@
+numbers_in_letters = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3hree",
+    "four": "f4ur",
+    "five": "f5ve",
+    "six": "s6x",
+    "seven": "s7ven",
+    "eight": "e8ght",
+    "nine": "n9ne",
+}
+
 with open("advent_1_input.txt", "r") as file:
     total = 0
     for line in file:
+        for letter, number in numbers_in_letters.items():
+            line = line.replace(letter, number)
         for char in line:
             if char.isdigit():
                 first = int(char)
@@ -10,4 +24,5 @@ with open("advent_1_input.txt", "r") as file:
                 second = int(char)
                 break
         total += first * 10 + second
+
     print(total)
--- advent_2.2.py
+++ advent_2.2.py
@@ -1,7 +1,4 @@
 cubes_per_game_map = {}
-
-cubes_per_game_map = {}
-
 with open("advent_2_input.txt", "r") as file:
     for line in file:
         id = int(line.split("Game ")[1].split(":")[0])
@@ -16,21 +13,22 @@ with open("advent_2_input.txt", "r") as file:
                         else parse_color
                     )
                     if id in cubes_per_game_map:
-                        cubes_per_game_map[id][color] += "," + value if value else ""
+                        cubes_per_game_map[id][color] += "," + value if value else "0"
                     else:
                         cubes_per_game_map[id] = {"blue": "0", "red": "0", "green": "0"}
                         cubes_per_game_map[id][color] = value
 
-thresholds = {"blue": 14, "red": 12, "green": 13}
-for id in list(cubes_per_game_map.keys()):
-    if any(
-        int(value) > thresholds[color]
-        for color in ["blue", "red", "green"]
-        for value in cubes_per_game_map[id][color].split(",")
-        if value
-    ):
-        cubes_per_game_map.pop(id)
+final_min_sum = 0
+for id in cubes_per_game_map.keys():
+    max_blue = max(
+        int(value) for value in cubes_per_game_map[id]["blue"].split(",") if value
+    )
+    max_red = max(
+        int(value) for value in cubes_per_game_map[id]["red"].split(",") if value
+    )
+    max_green = max(
+        int(value) for value in cubes_per_game_map[id]["green"].split(",") if value
+    )
+    final_min_sum += max_blue * max_red * max_green
 
-
-final_sum = sum(cubes_per_game_map.keys())
-print(final_sum)
+print(final_min_sum)
